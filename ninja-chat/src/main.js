62b856d65471faa724be4b37cd5decb5fb0385c3
Mehmet Sonmezates <sonmezatesmehmet@gmail.com>
2018-08-24 12:49:27 -0500

    Add scroll bar feature into chat page

--- ninja-chat/src/main.js
+++ ninja-chat/src/main.js
@@ -1,6 +1,9 @@
 import Vue from "vue";
 import router from "./router";
 import App from "./App.vue";
+import VueChatScroll from "vue-chat-scroll";
+
+Vue.use(VueChatScroll);
 
 Vue.config.productionTip = false;
 
