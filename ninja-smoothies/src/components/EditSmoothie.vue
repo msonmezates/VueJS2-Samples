21d80370c73d305ae31e3f76f73198ac999a14d5
Mehmet Sonmezates <sonmezatesmehmet@gmail.com>
2018-08-24 06:54:45 -0500

    Retrieve data from database in edit page and render title to the screen

--- ninja-smoothies/src/components/EditSmoothie.vue
+++ ninja-smoothies/src/components/EditSmoothie.vue
@@ -1,14 +1,30 @@
 <template>
-  <div class="edit-smoothie container">
-    <h2>Edit a smoothie {{ this.$route.params.smoothie_slug }}</h2>
+  <div class="edit-smoothie container" v-if="smoothie">
+    <h2>Edit {{ smoothie.title }}</h2>
   </div>
 </template>
 
 <script>
+import db from "@/firebase/init";
+
 export default {
   name: "EditSmoothie",
   data() {
-    return {};
+    return {
+      smoothie: null
+    };
+  },
+  created() {
+    let ref = db
+      .collection("smoothies")
+      .where("slug", "==", this.$route.params.smoothie_slug);
+
+    ref.get().then(snapshot => {
+      snapshot.forEach(doc => {
+        this.smoothie = doc.data();
+        this.smoothie.id = doc.id;
+      });
+    });
   }
 };
 </script>
