9a6215911305e42748763273d10b2660498c9e3f
Mehmet Sonmezates <sonmezatesmehmet@gmail.com>
2018-08-24 11:56:20 -0500

    Add entered text into database

--- ninja-chat/src/components/NewMessage.vue
+++ ninja-chat/src/components/NewMessage.vue
@@ -4,11 +4,14 @@
     <form @submit.prevent="addMessage">
       <label for="new-message">New Message (enter to add):</label>
       <input type="text" name="new-message" v-model="newMessage">
+      <p class="red-text" v-if="feedback">{{ feedback }}</p>
     </form>
   </div>
 </template>
 
 <script>
+import db from "@/firebase/init";
+
 export default {
   name: "NewMessage",
   props: {
@@ -16,12 +19,27 @@ export default {
   },
   data() {
     return {
-      newMessage: null
+      newMessage: null,
+      feedback: null
     };
   },
   methods: {
     addMessage() {
-      console.log(this.newMessage, this.name, Date.now());
+      if (this.newMessage) {
+        db
+          .collection("messages")
+          .add({
+            name: this.name,
+            content: this.newMessage,
+            timestamp: Date.now()
+          })
+          .catch(err => console.log(err));
+
+        this.newMessage = null;
+        this.feedback = null;
+      } else {
+        this.feedback = "Please enter a message";
+      }
     }
   }
 };
