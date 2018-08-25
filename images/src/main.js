d84093c87daf22d796e3f851007fa10b93a71b8f
Mehmet Sonmezates <sonmezatesmehmet@gmail.com>
2018-08-25 12:14:52 -0500

    Set up vue router

--- images/src/main.js
+++ images/src/main.js
@@ -1,8 +1,24 @@
 import Vue from "vue";
+import Router from 'vue-router';
 import App from "./App";
 import store from "./store";
+import AuthHandler from '@/components/AuthHandler';
+
+Vue.use(Router);
+
+const router = new Router({
+  mode: 'history',
+  routes: [
+    {
+      path: '/oauth2/callback',
+      name: 'AuthHandler',
+      component: AuthHandler
+    }
+  ]
+});
 
 new Vue({
+  router,
   store,
   render: h => h(App)
 }).$mount("#app");
