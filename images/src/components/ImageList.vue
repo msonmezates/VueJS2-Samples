7f61526cf10071626bdf0fe30b9df2c691293eef
Mehmet Sonmezates <sonmezatesmehmet@gmail.com>
2018-08-26 10:11:55 -0500

    Style uploaded images and add a message if user is not logged in

--- images/src/components/ImageList.vue
+++ images/src/components/ImageList.vue
@@ -1,6 +1,9 @@
 <template>
   <div>
-    <img v-for="(image,i) in allImages" :key="i" :src="image.link" />
+    <div class="image-container" v-if="isLoggedIn">
+      <img v-for="(image,i) in allImages" :key="i" :src="image.link" />
+    </div>
+    <h2 v-else>You must log in to get started!</h2>
   </div>
 </template>
 
@@ -9,10 +12,22 @@ import { mapActions, mapGetters } from "vuex";
 
 export default {
   name: "ImageList",
-  computed: mapGetters(["allImages"]),
+  computed: mapGetters(["allImages", "isLoggedIn"]),
   methods: mapActions(["fetchImages"]),
   created() {
     this.fetchImages();
   }
 };
 </script>
+
+<style scoped>
+.image-container {
+  column-count: 3;
+  column-gap: 0;
+}
+
+img {
+  max-width: 100%;
+  padding: 5px;
+}
+</style>
