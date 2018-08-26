0cd122bd1f2cbf7866b80aa8a117451cc3321478
Mehmet Sonmezates <sonmezatesmehmet@gmail.com>
2018-08-26 13:53:24 -0500

    Remove boilerplate, create new components and display basic navbar with router

---
+++ geo-ninjas/src/components/layout/Navbar.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="navbar">
+    <nav class="deep-purple darken-1">
+      <div class="container">
+        <a href="" class="brand-logo left">GoeNinjas</a>
+        <ul class="right">
+          <li><a href="">Sign Up</a></li>
+          <li><a href="">Login</a></li>
+        </ul>
+      </div>
+    </nav>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Navbar",
+  data() {
+    return {};
+  }
+};
+</script>
+
+<style>
+</style>
---
+++ geo-ninjas/src/firebase/init.js
@@ -0,0 +1,15 @@
+import firebase from 'firebase';
+import firestore from 'firebase/firestore';
+
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyAPOiVqJvVEa2j2o_UILxgFc1P4f150YqM",
+  authDomain: "vue-geo-ninjas-e5a00.firebaseapp.com",
+  databaseURL: "https://vue-geo-ninjas-e5a00.firebaseio.com",
+  projectId: "vue-geo-ninjas-e5a00",
+  storageBucket: "",
+  messagingSenderId: "22890645296"
+};
+const firebaseApp = firebase.initializeApp(config);
+
+export default firebaseApp.firestore();
