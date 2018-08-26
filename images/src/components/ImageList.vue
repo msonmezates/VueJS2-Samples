959bfedce986e71ea14349df16e50803f8a8eee6
Mehmet Sonmezates <sonmezatesmehmet@gmail.com>
2018-08-25 22:18:16 -0500

    Fetch images from api and list it in imagelist page

--- images/src/components/ImageList.vue
+++ images/src/components/ImageList.vue
@@ -1,14 +1,15 @@
 <template>
   <div>
-    Image List
+    <img v-for="(image,i) in allImages" :key="i" :src="image.link" />
   </div>
 </template>
 
 <script>
-import { mapActions } from "vuex";
+import { mapActions, mapGetters } from "vuex";
 
 export default {
   name: "ImageList",
+  computed: mapGetters(["allImages"]),
   methods: mapActions(["fetchImages"]),
   created() {
     this.fetchImages();
