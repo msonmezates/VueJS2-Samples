174887ae53480fa0826a2bbf4c5d4482a36e3afe
Mehmet Sonmezates <sonmezatesmehmet@gmail.com>
2018-08-25 22:00:58 -0500

    Create vuex store for images, wire it up to imagelist page and get a response when fetch request is made

--- images/src/api/imgur.js
+++ images/src/api/imgur.js
@@ -1,4 +1,5 @@
 import qs from "qs";
+import axios from 'axios';
 
 const CLIENT_ID = "57c76613ffecee9";
 const ROOT_URL = "https://api.imgur.com";
@@ -11,5 +12,13 @@ export default {
     };
     // window.location takes user to the url
     window.location = `${ROOT_URL}/oauth2/authorize?${qs.stringify(queryString)}`;
+  },
+  fetchImages(token) {
+    return axios.get(`${ROOT_URL}/3/account/me/images`, {
+      headers: {
+        Authorization: `Bearer ${token}`
+      }
+    })
   }
 };
+
