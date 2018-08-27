f932ed57e176ecc9c3b3428918ed638be49634f7
Mehmet Sonmezates <sonmezatesmehmet@gmail.com>
2018-08-27 07:55:54 -0500

    Add a signout button in navbar and implement its functionality to log user out and redirect to signup page

--- geo-ninjas/src/components/layout/Navbar.vue
+++ geo-ninjas/src/components/layout/Navbar.vue
@@ -4,8 +4,9 @@
       <div class="container">
         <a href="" class="brand-logo left">GoeNinjas</a>
         <ul class="right">
-          <li><a href="">Sign Up</a></li>
+          <li><router-link :to="{ name: 'Signup' }">Signup</router-link></li>
           <li><a href="">Login</a></li>
+          <li><a @click="logout">Logout</a></li>
         </ul>
       </div>
     </nav>
@@ -13,10 +14,22 @@
 </template>
 
 <script>
+import firebase from "firebase";
+
 export default {
   name: "Navbar",
   data() {
     return {};
+  },
+  methods: {
+    logout() {
+      firebase
+        .auth()
+        .signOut()
+        .then(() => {
+          this.$router.push({ name: "Signup" });
+        });
+    }
   }
 };
 </script>
