fe99b6ebe67c3632ec65162695c1b0801e89b51c
Mehmet Sonmezates <sonmezatesmehmet@gmail.com>
2018-08-27 05:57:55 -0500

    Create signup page with a form

---
+++ geo-ninjas/src/components/auth/Signup.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="signup container">
+    <form class="card-panel">
+      <h2 class="center deep-purple-text">Signup</h2>
+      <div class="field">
+        <label for="email">Email:</label>
+        <input type="email" name="email" v-model="email">
+      </div>
+      <div class="field">
+        <label for="password">Password:</label>
+        <input type="password" name="password" v-model="password">
+      </div>
+      <div class="field">
+        <label for="alies">Alies:</label>
+        <input type="text" name="alies" v-model="alias">
+      </div>
+      <div class="field center">
+        <button class="btn deep-purple">Signup</button>
+      </div>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Signup",
+  data() {
+    return {
+      email: null,
+      password: null,
+      alias: null
+    };
+  }
+};
+</script>
+
+<style>
+.signup {
+  max-width: 400px;
+  margin-top: 60px;
+}
+
+.signup h2 {
+  font-size: 2.4em;
+}
+
+.signup .field {
+  margin-bottom: 16px;
+}
+</style>
--- geo-ninjas/src/router/index.js
+++ geo-ninjas/src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 import GoogleMap from '@/components/home/GoogleMap';
+import Signup from '@/components/auth/Signup';
 
 Vue.use(Router);
 
@@ -10,6 +11,11 @@ export default new Router({
       path: '/',
       name: 'GoogleMap',
       component: GoogleMap
+    },
+    {
+      path: '/signup',
+      name: 'Signup',
+      component: Signup
     }
   ]
 });
